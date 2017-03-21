179ad1727f5d709d8d3aab59592e6209d894018f
Kiril Vangelovski <kiril@hbee.eu>
2017-03-21 15:14:00 +0100

    backup

--- lib/ingester/odoo_logs.ex
+++ lib/ingester/odoo_logs.ex
@@ -1,11 +1,41 @@
 defmodule Ingester.OdooLogs do
 
   @moduledoc """
-  Process odoo logs
+  Process odoo logs in a directory
+  by creating a map (currently shows requests in a day)
   """
 
-  def process(logs_path) do
-    
+  def get_files(logs_path) do
+    File.ls!(logs_path)
+    |> Enum.filter(&(not File.dir?(&1)))
+    |> Enum.map(&Path.absname(&1, logs_path))
+  end
+
+  def update_map(map, match) do
+    case match do
+      %{"date" => date, "db" => db} -> Map.update(map, db<>"_"<>date, 1, &(&1 + 1))
+      _ -> map
+    end
+  end
+
+  def process({:path, logs_path}) do
+    get_files(logs_path)
+    |> Enum.map(&process({:file, &1}))
+    |> Enum.reduce(%{}, &(Map.merge(&1, &2, fn _, v1, v2 -> v1 + v2 end)))
+  end
+
+  def process({:file, file}) do
+    File.stream!(file)
+    |> Stream.map(&process({:line, &1}))
+    |> Enum.reduce(%{}, fn match, acc -> update_map(acc, match) end)
+  end
+
+  def process({:line, line}) do
+    case Regex.named_captures(
+          ~r/(?<date>^\S+).*\s(?<db>\S+)\swerkzeug/, line) do
+      nil -> %{}
+      match -> match
+    end
   end
   
 end
